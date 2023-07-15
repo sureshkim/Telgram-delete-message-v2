b94c23d5c13b2b4c26cd7957da8113519d2913d5
SURESH KIM <136443686+sureshkim@users.noreply.github.com>
2023-07-16 02:30:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,19 @@
 import os
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
+from telegram import Update, Message
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from telethon import TelegramClient
 from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.types import Channel
 
 def start(update: Update, context: CallbackContext) -> None:
     """Start command handler."""
-    update.message.reply_text('Bot started. I will automatically delete messages in every chat where I am added, except for videos (MKV, MP4).')
+    update.message.reply_text('Bot started.')
 
-def handle_message(update: Update, context: CallbackContext) -> None:
-    """Handle incoming messages."""
+def handle_text_message(update: Update, context: CallbackContext) -> None:
+    """Handle incoming text messages."""
     message = update.message
+    # Process and handle text messages here
 
-    if message.chat.type == 'channel':
-        channel_id = message.chat.id
-        client = TelegramClient('session_name', API_ID, API_HASH)
-        client.start()
+def handle_media_message(update: Update, context: CallbackContext) -> None:
 
-        try:
-            channel = client(GetFullChannelRequest(channel=channel_id))
-            if isinstance(channel.chats[0], Channel):
-                client.delete_messages(entity=channel.chats[0], message_ids=[message.message_id])
-        except Exception as e:
-            print(f'Error deleting message in channel: {str(e)}')
-        finally:
-            client.disconnect()
-
-def main() -> None:
-    """Main function to run the bot."""
-    token = os.environ.get('BOT_TOKEN')
-    updater = Updater(token, use_context=True)
-    dispatcher = updater.dispatcher
-
-    # Register command handlers
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(MessageHandler(Filters.all & ~Filters.command, handle_message))
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
-    
+                        
